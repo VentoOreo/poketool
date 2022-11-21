4f8072c33910835bd64e6bc6fbe899febe912946
Vento <squaredTrinity@gmail.com>
2022-11-21 13:40:33 -0600

    Add documentation to Row component

--- src/Components/RowComponents/Row.js
+++ src/Components/RowComponents/Row.js
@@ -1,3 +1,16 @@
+/**
+ * 
+ * @param {*} props 
+ * @param {*} props.rowKey - Unique key for each item
+ * @param {*} props.iterable - Iterable object to pass to the map function
+ * @param {*} props.colClass - The CSS class for each cell
+ * @param {*} props.getBg - Callback function to generate the background color of each cell
+ * @param {*} props.getValue - Callback function to generate the text in each cell
+ * @param {*} [props.innerOrder] - Optional order value for the flexbox
+ * @param {*} [props.header] - Optional header object to pass as the first object in the row
+ * @returns div flexbox object containing a list of child divs
+ */
+
 function Row(props){
     return(<div className="grid" key={props.rowKey} style={{order:(props.innerOrder? props.innerOrder : 1)}}>
       {props.header}
